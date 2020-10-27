709c5ea0f43e805b267fa7cc679885f395ae9fd8
zhangweilai <zhangweilai@galaxymx.com>
2020-10-27 19:54:01 +0800

    增加苹果web登录

---
+++ src/Providers/AppleProvider.php
@@ -0,0 +1,118 @@
+<?php
+
+/*
+ * This file is part of the overtrue/socialite.
+ *
+ * (c) overtrue <i@overtrue.me>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace Overtrue\Socialite\Providers;
+
+use GuzzleHttp\ClientInterface;
+use Overtrue\Socialite\AccessTokenInterface;
+use Overtrue\Socialite\ProviderInterface;
+use Overtrue\Socialite\User;
+
+/**
+ * Class AppleProvider.
+ *
+ * @see https://developer.apple.com/documentation/sign_in_with_apple
+ */
+class AppleProvider extends AbstractProvider implements ProviderInterface
+{
+    /**
+     * The separating character for the requested scopes.
+     *
+     * @var string
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * The scopes being requested.
+     *
+     * @var array
+     */
+    protected $scopes = [
+       'name',
+       'email'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase('https://appleid.apple.com/auth/authorize', $state);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://appleid.apple.com/auth/token';
+    }
+
+    /**
+     * Get the access token for the given code.
+     *
+     * @param string $code
+     *
+     * @return string
+     */
+    public function getAccessToken($code)
+    {
+        $postKey = (1 === version_compare(ClientInterface::VERSION, '6')) ? 'form_params' : 'body';
+
+        $response = $this->getHttpClient()->post($this->getTokenUrl(), [
+            $postKey => $this->getTokenFields($code),
+        ]);
+
+        return $this->parseAccessToken($response->getBody());
+    }
+
+    /**
+     * Get the POST fields for the token request.
+     *
+     * @param string $code
+     *
+     * @return array
+     */
+    protected function getTokenFields($code)
+    {
+        return parent::getTokenFields($code) + ['grant_type' => 'authorization_code'];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken(AccessTokenInterface $token)
+    {
+        $response = $this->getHttpClient()->get('https://www.googleapis.com/userinfo/v2/me', [
+            'headers' => [
+                'Accept' => 'application/json',
+                'Authorization' => 'Bearer '.$token->getToken(),
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return new User([
+            'id' => $this->arrayItem($user, 'id'),
+            'username' => $this->arrayItem($user, 'email'),
+            'nickname' => $this->arrayItem($user, 'name'),
+            'name' => $this->arrayItem($user, 'name'),
+            'email' => $this->arrayItem($user, 'email'),
+            'avatar' => $this->arrayItem($user, 'picture'),
+        ]);
+    }
+}
